b565c8bc7c2e15de29d78616e0c3bbc38de811fb
Gabriel Arpini <arpinigabrielsr71@gmail.com>
2025-03-23 17:17:07 +0000

    finished models

--- models/core/orbit_debris.sql
+++ models/core/orbit_debris.sql
@@ -9,11 +9,10 @@ WITH ORBIT_DEBRIS AS (
         o.PERIOD,
         o.INCLINATION,
         o.LAUNCH_DATE,
-        c.ORBIT_CAT,
         {{ orbit_category('ECCENTRICITY', 'PERIOD', 'INCLINATION', 'SEMIMAJOR_AXIS') }} AS ORBIT_CAT
-        ON o.NORAD_CAT_ID = c.NORAD_CAT_ID
-    WHERE (o.OBJECT_TYPE = 'DEBRIS' OR o.OBJECT_TYPE = 'ROCKET BODY') 
-    AND o.DECAY_DATE IS NULL
+        FROM {{ source('project_zoomcamp', 'spacetrack_gp_data') }} AS o
+        WHERE (o.OBJECT_TYPE = 'DEBRIS' OR o.OBJECT_TYPE = 'ROCKET BODY') 
+        AND o.DECAY_DATE IS NULL
 ),
 
 ORBIT_CATEGORY_COUNTS AS (
